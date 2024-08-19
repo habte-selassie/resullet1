99d5315bb529158f92b89d9198ac6669cfb81502
JW-dev0505 <shcherbakarina6@gmail.com>
2024-08-19 12:51:20 +0900

    add login & register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { MetaMaskProvider } from "@metamask/sdk-react";
-import { ResumePage, Home } from './screens';
+import { ResumePage, Home, Login, Register } from './screens';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import './App.css';
@@ -28,16 +28,18 @@ class App extends Component {
               <Link to="/" className="brand">RESULLET</Link>
               <div className="links">
                 <Link to="/" className="home-link">Home</Link>
-                <Link to="/appa" className="appa-link">Appa</Link>
-                <Link to="/login" className="login-link">Login</Link>
                 <Link to="/resume" className="resume-link">Resume</Link>
+                {/* <Link to="/appa" className="appa-link">Appa</Link> */}
+                <Link to="/login" className="login-link">Login</Link>
+                <Link to="/regist" className="login-link">Register</Link>
               </div>
             </nav>
     
             <Routes>
               <Route path="/" element={<Home />} />
               {/* <Route path="/appa" element={<About />} /> */}
-              <Route path="/login" element={<></>} /> {/* login */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/regist" element={<Register />} />
               <Route path="/resume" element={<ResumePage />} />
             </Routes>
           </Router>
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,2 +1,4 @@
 export { default as ResumePage } from './ResumePage/ResumePage'
-export { default as Home } from './Home'
+export { default as Home } from './Home/Home'
+export { default as Login } from './Login/Login'
+export { default as Register } from './Register/register'
