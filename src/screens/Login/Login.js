9392c63f16622b6fc47764ecd153bba8270040c1
Marcelo Pignataro <pignataromd@gmail.com>
2024-08-19 13:00:21 +0100

    google-auth implemented, test in production

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,78 +1,91 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { supabase } from "@supabase/auth-ui-shared";
 
-import { supabase } from '@supabase/auth-ui-shared'
-
+// import ConnectButton from "Wallets/walletConnect-integration/walletConnect";
 //import { supabase } from '../supabaseClient';
 
-
-  // Import the Supabase client
+// Import the Supabase client
 
 async function createNonce() {
-  const nonce = btoa(String.fromCharCode(...crypto.getRandomValues(new Uint8Array(32))));
-  const encoder = new TextEncoder();
-  const encodedNonce = encoder.encode(nonce);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', encodedNonce);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashedNonce = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
-  return { nonce, hashedNonce };
+	const nonce = btoa(
+		String.fromCharCode(...crypto.getRandomValues(new Uint8Array(32)))
+	);
+	const encoder = new TextEncoder();
+	const encodedNonce = encoder.encode(nonce);
+	const hashBuffer = await crypto.subtle.digest("SHA-256", encodedNonce);
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+	const hashedNonce = hashArray
+		.map((b) => b.toString(16).padStart(2, "0"))
+		.join("");
+	return { nonce, hashedNonce };
 }
 
 export default function Login() {
-  const [nonce, setNonce] = useState('');
+	const [nonce, setNonce] = useState("");
 
-  useEffect(() => {
-    async function generateNonce() {
-      const { nonce } = await createNonce();
-      setNonce(nonce);  // Set the nonce in the state
-    }
+	useEffect(() => {
+		async function generateNonce() {
+			const { nonce } = await createNonce();
+			setNonce(nonce); // Set the nonce in the state
+		}
 
-    generateNonce();
-  }, []);
+		generateNonce();
+	}, []);
 
-  async function handleSignInWithGoogle(response) {
-    const { data, error } = await supabase.auth.signInWithIdToken({
-      provider: 'google',
-      token: response.credential,
-      nonce: nonce,
-    });
+	async function handleSignInWithGoogle(response) {
+		const { data, error } = await supabase.auth.signInWithIdToken({
+			provider: "google",
+			token: response.credential,
+			nonce: nonce,
+		});
 
-    if (error) {
-      console.error('Error signing in with Google:', error);
-    } else {
-      console.log('Signed in successfully:', data);
-    }
-  }
+		if (error) {
+			console.error("Error signing in with Google:", error);
+		} else {
+			console.log("Signed in successfully:", data);
+		}
+	}
 
-  useEffect(() => {
-    window.handleSignInWithGoogle = handleSignInWithGoogle;  // Expose the function to the global scope
-  }, []);
+	useEffect(() => {
+		window.handleSignInWithGoogle = handleSignInWithGoogle; // Expose the function to the global scope
+	}, []);
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <div id="g_id_onload"
-           data-client_id="resullet"
-           data-context="signin"
-           data-ux_mode="popup"
-           data-callback="handleSignInWithGoogle"
-           data-nonce={nonce}
-           data-auto_select="true"
-           data-itp_support="true"
-           data-use_fedcm_for_prompt="true">
-      </div>
+	const responseMessage = (response) => {
+		console.log(response);
+	};
+	const errorMessage = (error) => {
+		console.log("the error:", error);
+	};
 
-      <div className="g_id_signin"
-           data-type="standard"
-           data-shape="pill"
-           data-theme="filled_blue"
-           data-text="signin_with"
-           data-size="large"
-           data-logo_alignment="left">
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			{/* <div
+				id="g_id_onload"
+				data-client_id="resullet"
+				data-context="signin"
+				data-ux_mode="popup"
+				data-callback="handleSignInWithGoogle"
+				data-nonce={nonce}
+				data-auto_select="true"
+				data-itp_support="true"
+				data-use_fedcm_for_prompt="true"
+			></div> */}
+			{/* <ConnectButton /> */}
+			<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+
+			{/* <div
+				className="g_id_signin"
+				data-type="standard"
+				data-shape="pill"
+				data-theme="filled_blue"
+				data-text="signin_with"
+				data-size="large"
+				data-logo_alignment="left"
+			></div> */}
+		</div>
+	);
 }
-
 
 //import { supabase } from '@supabase/auth-ui-shared'
 // import React, { useEffect, useState } from 'react'
